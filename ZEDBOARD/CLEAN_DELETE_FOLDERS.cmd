e502dfaa8b3af7deb64770aa85e583055b2f4ab5
Sakinder <sakinder@live.com>
2019-10-19 06:42:51 -0400

    rebuild image bin file

--- ZEDBOARD/CLEAN_DELETE_FOLDERS.cmd
+++ ZEDBOARD/CLEAN_DELETE_FOLDERS.cmd
@@ -93,17 +93,16 @@ if "%creatProject%"=="n" (GOTO EOF)
 @set vlog_folder=%batchfile_path%v_log
 @set vlog_folderv=%batchfile_path%vivado
 
-@set bkworkspace_folder=%batchfile_path%bkworkspace
+@set bkworkspace_folder=%batchfile_path%backup_workspace
 @set workspace_folder=%batchfile_path%workspace
 REM REMOVE LOG FOLDER
 @echo REMOVE BKWORKSPACE_FOLDER FOLDER
 
 @if exist %workspace_folder% ( @echo -- REMOVE OLD VLOG FILES AND FOLDER --
-@rd %bkworkspace_folder% /s /q 
-xcopy "%batchfile_path%\workspace\sdk\fsbl\*.*" "%batchfile_path%\bkworkspace\sdk\fsbl\" >nul /E /K /D /H /Y
-xcopy "%batchfile_path%\workspace\sdk\fsbl_bsp\*.*" "%batchfile_path%\bkworkspace\sdk\fsbl_bsp\" >nul /E /K /D /H /Y
-xcopy "%batchfile_path%\workspace\sdk\vfpWebCamera\*.*" "%batchfile_path%\bkworkspace\sdk\vfpWebCamera\" >nul /E /K /D /H /Y
-xcopy "%batchfile_path%\workspace\sdk\vfpWebCamera_bsp\*.*" "%batchfile_path%\bkworkspace\sdk\vfpWebCamera_bsp\" >nul /E /K /D /H /Y
+xcopy "%batchfile_path%\workspace\sdk\fsbl\*.*" "%batchfile_path%\backup_workspace\SDK\fsbl\" >nul /E /K /D /H /Y
+xcopy "%batchfile_path%\workspace\sdk\fsbl_bsp\*.*" "%batchfile_path%\backup_workspace\SDK\fsbl_bsp\" >nul /E /K /D /H /Y
+xcopy "%batchfile_path%\workspace\sdk\vfpWebCamera\*.*" "%batchfile_path%\backup_workspace\SDK\vfpWebCamera\" >nul /E /K /D /H /Y
+xcopy "%batchfile_path%\workspace\sdk\vfpWebCamera_bsp\*.*" "%batchfile_path%\backup_workspace\SDK\vfpWebCamera_bsp\" >nul /E /K /D /H /Y
 )
 
 
